4d6d06c0790dc34388a937a641fd6ebbcd1afe80
unnameduser95 <unnameduser95@pm.me>
2020-05-17 21:11:58 -0700

    remove junk code

--- background.js
+++ background.js
@@ -4,9 +4,6 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 })
 
-// let isSecondLoad = false;
-// let originalUrl = "";
-
 let isRunning = true;
 
 chrome.storage.sync.get("running", function(data) {
@@ -25,36 +22,3 @@ chrome.webNavigation.onBeforeNavigate.addListener(function(details) {  // change
     chrome.tabs.update(details.tabId, {url: myUrl});
   }
 })
-
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {  // nightmare nightmare nightmare nightmare nightmare nightmare nightmare nightmare nightmare nightmare 
-//   console.log("tabId", tabId)
-//   console.log("changeInfo status", changeInfo.status);
-//   if (isRunning) {
-//     const tabUrl = tab.url;
-//     let isVideo = tabUrl.startsWith("https://www.youtube.com/watch") ? true : false;
-//     // console.log("isVideo", isVideo);
-//     // console.log("isSecondLoad", isSecondLoad);
-//     // console.log("originalUrl", originalUrl);
-    
-//     if (tabUrl.startsWith("https://www.youtube.com") && !tabUrl.endsWith("disable_polymer=1")) {
-//       // console.log("tabUrl", tabUrl);
-//       let myUrl = tabUrl.indexOf("?") > -1 ? tabUrl.concat("&disable_polymer=1") : tabUrl.concat("?disable_polymer=1");
-
-//       if (!isVideo || originalUrl !== tabUrl) {
-//         isSecondLoad = false;
-//         originalUrl = "";
-//       }
-
-//       if (changeInfo.status !== "complete" && isSecondLoad === false) {
-//         // console.log("Performing refresh");
-//         chrome.tabs.update(tabId, {url: myUrl});
-//       }
-
-//       if (isVideo) {
-//         // console.log("Running second load");
-//         isSecondLoad = true;
-//         originalUrl = tabUrl
-//       }
-//     };
-//   }
-// })
