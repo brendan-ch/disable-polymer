69aaaee425bb002367af33c5032c5f7ad47104b0
unnameduser95 <unnameduser95@pm.me>
2020-05-17 20:55:20 -0700

    redesign popup

--- popup.js
+++ popup.js
@@ -1,30 +1,45 @@
-let button = document.getElementById('toggleButton');
-let text = document.getElementById('text');
+let polymer = document.getElementById('polymer-button');
+let classic = document.getElementById('classic-button');
 
 chrome.storage.sync.get("running", (data) => {
-  if (data.running) {
-    button.innerText = "Turn off";
-    text.innerText = "This extension is currently running.";
-  } else {
-    button.innerText = "Turn on";
-    text.innerText = "This extension is currently disabled.";
-  }
+  classic.style.backgroundColor = data.running ? "#c4d9ff" : "#ffffff";
+  polymer.style.backgroundColor = data.running ? "#ffffff" : "#c4d9ff";
+
+  // classic.style.borderColor = data.running ? "#005bff" : '#ffffff';
+  // polymer.style.borderColor = data.running ? "#ffffff" : "#005bff";
 })
 
-button.onclick = () => {
-  chrome.storage.sync.get("running", (data) => {
-    if (data.running) {
-      button.innerText = "Turn on";
-      text.innerText = "This extension is currently disabled.";
-      chrome.storage.sync.set({running: false}, function () {
-        console.log("Extension is no longer running.")
-      })
-    } else {
-      button.innerText = "Turn off";
-      text.innerText = "This extension is currently running.";
-      chrome.storage.sync.set({running: true}, function () {
-        console.log("Extension is currently running.")
-      })
-    }
-  });
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  classic.style.backgroundColor = changes["running"].newValue ? "#c4d9ff" : "#ffffff";
+  polymer.style.backgroundColor = changes["running"].newValue ? "#ffffff" : "#c4d9ff";
+})
+
+polymer.onclick = () => {
+  chrome.storage.sync.set({running: false}, function () {
+    console.log("Polymer mode saved.")
+  })
 }
+
+classic.onclick = () => {
+  chrome.storage.sync.set({running: true}, function () {
+    console.log("Classic mode saved.")
+  })
+}
+
+// button.onclick = () => {
+//   chrome.storage.sync.get("running", (data) => {
+//     if (data.running) {
+//       button.innerText = "Turn on";
+//       text.innerText = "This extension is currently disabled.";
+//       chrome.storage.sync.set({running: false}, function () {
+//         console.log("Extension is no longer running.")
+//       })
+//     } else {
+//       button.innerText = "Turn off";
+//       text.innerText = "This extension is currently running.";
+//       chrome.storage.sync.set({running: true}, function () {
+//         console.log("Extension is currently running.")
+//       })
+//     }
+//   });
+// }
